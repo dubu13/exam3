d9a7ecc49678b58f3988737aab57634ea62ac1f4
dubu13 <dilara.hasan1301@gmail.com>
2024-05-15 21:09:37 +0200

    working, forgot to change int to long for %d

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int	print_s(char *str)
 {
-	int	count = 0;
+	int count = 0;
 
 	if (!str)
 		str = "(null)";
@@ -13,48 +13,47 @@ int	print_s(char *str)
 	return (count);
 }
 
-int	print_d(unsigned int num)
+int	print_d(int num)
 {
+	long long number = num;
 	int count = 0;
 	char n;
 
-	if (num < 0)
+	if (number < 0)
 	{
-		num *= -1;
-		write(1, "-", 1);
-		count++;
+		number *= -1;
+		count += write(1, "-", 1);
 	}
-	if (num >= 10)
-		count += print_d(num / 10);
-	n = "0123456789"[num % 10];
-	write(1, &n, 1);
-	count++;
+	if (number >= 10)
+		count += print_d(number / 10);
+	n = "0123456789"[number % 10];
+	count += write(1, &n, 1);
 	return (count);
 }
 
 int	print_x(unsigned int num)
 {
-	int	count = 0;
+	int count = 0;
 	char n;
 
 	if (num >= 16)
 		count += print_x(num / 16);
 	n = "0123456789abcdef"[num % 16];
-	write(1, &n, 1);
-	count++;
+	count += write(1, &n, 1);
 	return (count);
 }
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	arg;
-	int		count = 0;
-
+	int	count = 0;
 	va_start(arg, format);
+
 	if (!format)
 		return (-1);
 	while (*format != '\0')
 	{
-		if ((*format == '%') && (*(format + 1) == 's' || *(format + 1) == 'd' || *(format + 1) == 'x'))
+		if (*format == '%' && (*(format + 1) == 's' || *(format + 1) == 'd' || *(format + 1) == 'x'))
 		{
 			format++;
 			if (*format == 's')
@@ -73,20 +72,19 @@ int	ft_printf(const char *format, ...)
 }
 
 // #include <stdio.h>
-
 // int main()
 // {
 // 	printf("Testing print_s:\n");
 // 	ft_printf("Hello %s!\n", "world"); // Should print: Hello world!
 
 // 	printf("\nTesting print_d:\n");
-// 	ft_printf("The number is %d.\n", 42);
+// 	ft_printf("The number is %d.\n", -2147483648); // Should print: The number is 42.
 
 // 	printf("\nTesting print_x:\n");
 // 	ft_printf("The hexadecimal number is %x.\n", 255); // Should print: The hexadecimal number is ff.
 
 // 	printf("\nTesting null string:\n");
-// 	ft_printf("This is a %s test.\n", NULL);
+// 	ft_printf("This is a %s test.\n", NULL); // Should print: This is a (null) test.
 
 // 	return 0;
 // }
